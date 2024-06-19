1c0ad5e1c294781d50da6cf40de48ab15fe813e0
DevPhamPham <pham.duykhoa1303@gmail.com>
2024-06-20 00:01:23 +0700

    update Bảo

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
     ],
     imgs: {
         "Chàng trai đáng yêu của tôi": "https://scontent.fsgn6-2.fna.fbcdn.net/v/t39.30808-6/391641742_3583648591956875_9126676818289576801_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=pud0udVIVRkQ7kNvgH3HRTS&_nc_ht=scontent.fsgn6-2.fna&oh=00_AYD4G7pl_5BU8Jq4Tgeg-egQyHKxu-KUS8yDpyRbard-_Q&oe=6678E102",
-        "Hôm nay là sinh nhật lần thứ 21 của Bảo": "./imgs/ae2.jpg",
+        "Hôm nay là sinh nhật lần thứ 21 của Bảo": "./imgs/ae1.jpg",
         "Chúc Bảo có một ngày sinh nhật tràn đầy niềm vui và hạnh phúc, bên cạnh gia đình, bạn bè, và những người thân yêu nhất.": "./imgs/ae2.jpg",
     },
     desc: {
